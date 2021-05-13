1c58b72afd812a2e91f6c2876892e51ff375c08e
Bernardo <bernardo_filipe89@hotmail.com>
2021-05-14 00:11:33 +0100

    add validation

--- src/Controller/EmissionController.php
+++ src/Controller/EmissionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,13 +15,26 @@ class EmissionController extends AbstractController
     public function index(): Response
     {
 
+        try{
 
-        $url = 'https://events.scoocs.co/public/total_co2';
-        $response = file_get_contents($url);
-         $json_array=json_decode($response,true); 
-        $emissionSaved=$json_array["total_co2"];
-        return $this->render('emission/index.html.twig', [
-            'number' => $emissionSaved,
-        ]);
+            $url = 'https://events.scoocs.co/public/total_co2';
+            $response = file_get_contents($url);
+             $json_array=json_decode($response,true); 
+             $emissionSaved=0;
+             if($json_array["success"]){
+                $emissionSaved=$json_array["total_co2"];
+             }
+            
+            return $this->render('emission/index.html.twig', [
+                'number' => $emissionSaved,
+            ]);
+
+        }catch(Exception $exception ){
+
+            throw new $exception;
+
+        }
+
+     
     }
 }
